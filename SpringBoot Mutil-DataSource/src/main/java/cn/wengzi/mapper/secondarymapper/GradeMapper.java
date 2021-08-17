07961cdc46d13c81615695b453d8f998fa0fa86a
leizige <lei.tl@bitsun-inc.com>
2021-08-17 19:52:29 +0800

    springboot 集成 redis

--- SpringBoot Mutil-DataSource/src/main/java/cn/wengzi/mapper/secondarymapper/GradeMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.wengzi.mapper.secondarymapper;
-
-import cn.wengzi.entity.secondary.Grade;
-
-import java.util.List;
-
-public interface GradeMapper {
-    List<Grade> findAll();
-}
--- SpringBoot-Excel/src/main/java/cn/wengzi/excel/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package cn.wengzi.excel.service;
-
-import cn.wengzi.excel.entity.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> QueryAllUser();
-}
