a9d283e608101da876a942e5fc9b67275fd7dd90
不颓废青年 <52662214+qx-leizige@users.noreply.github.com>
2021-08-17 17:37:02 +0800

    Update Application.java
    first springboot application

--- springboot/src/main/java/com/springboot/Application.java
+++ springboot/src/main/java/com/springboot/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+/**
+ * @author leizige
+ */
 @SpringBootApplication
 //@ImportResource(locations = {"classpath:spring.xml"})
 public class Application {
