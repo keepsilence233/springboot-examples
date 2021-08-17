b1269e699a5e8deb439b481e6cd50e1436ee6232
leizige <lei.tl@bitsun-inc.com>
2021-08-17 19:07:12 +0800

    springboot 发送邮件

--- springboot-email/src/main/java/cn/wengzi/Application.java
+++ springboot-email/src/main/java/cn/wengzi/Application.java
@@ -3,6 +3,10 @@ package cn.wengzi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author leizige
+ * created 2020/06/15
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
