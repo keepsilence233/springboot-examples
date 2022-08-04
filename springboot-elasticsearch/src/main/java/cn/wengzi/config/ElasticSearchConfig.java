0abdb29762eb5e325c25a2f784565bc31cf077ab
“leizige” <qx_leizige@163.com>
2022-08-04 16:35:02 +0800

    update es config

--- springboot-elasticsearch/src/main/java/cn/wengzi/config/ElasticSearchConfig.java
+++ springboot-elasticsearch/src/main/java/cn/wengzi/config/ElasticSearchConfig.java
@@ -1,7 +1,12 @@
 package cn.wengzi.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +22,12 @@ public class ElasticSearchConfig {
     @Autowired
     private ElasticSearchProperties elasticSearchProperties;
 
+
+    /**
+     * 无密码调用方式
+     *
+     * @return
+     */
     @Bean
     public RestHighLevelClient restHighLevelClient() {
         return new RestHighLevelClient(
@@ -26,4 +37,22 @@ public class ElasticSearchConfig {
                 )
         );
     }
+
+    /**
+     * 有密码调用方式
+     *
+     * @return
+     */
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("elasticsearch", "bitsun@es"));
+        return new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost(elasticSearchProperties.getAddress(),
+                                elasticSearchProperties.getPort(), elasticSearchProperties.getScheme())
+                ).setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+        );
+    }
+
 }
