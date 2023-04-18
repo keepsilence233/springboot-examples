7afae04216c76af753fc6f32947ddf2c17e1126d
leizige <52662214+keepsilence233@users.noreply.github.com>
2023-04-19 00:54:51 +0800

    add springboot xxl-job module

---
+++ springboot-xxl-job/src/main/java/qx/leizige/Application.java
@@ -0,0 +1,14 @@
+package qx.leizige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springBoot xxl-job
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
