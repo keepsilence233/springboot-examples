3cdafafe8df2e1ebd09f0727d9a2fe9cc63a1ae3
leizige <52662214+keepsilence233@users.noreply.github.com>
2023-04-19 00:53:13 +0800

    add springboot apollo module

---
+++ springboot-apollo/src/main/java/qx/leizige/Application.java
@@ -0,0 +1,14 @@
+package qx.leizige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springBoot apollo config center
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
